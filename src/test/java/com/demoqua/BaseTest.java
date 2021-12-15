c9bdc7a9bb3e1f61b5077251bb3718ec5ab80a5e
sunny <5olnce@mail.ru>
2021-12-15 17:06:46 +0200

    added attachments

--- src/test/java/com/demoqua/BaseTest.java
+++ src/test/java/com/demoqua/BaseTest.java
@@ -1,6 +1,8 @@
 package com.demoqua;
 
 import com.codeborne.selenide.Configuration;
+import helpers.Attach;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -10,8 +12,16 @@ public class BaseTest {
     public static void before() {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
+
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
     }
 }
